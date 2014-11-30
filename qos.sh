37df9fb0864d73801d0d52db1e19bca0290d4ead
Anthony Ruhier <anthony.ruhier@gmail.com>
2014-11-30 02:25:34 +0100

    Adapted for current setup
    Well, not fully adapted, but almost...

--- qos.sh
+++ qos.sh
@@ -26,29 +26,31 @@ case "$1" in
 
         # INTERACTIVE CLASS: for low latency, high priority packets such as VoIP and DNS
         # it has little bandwidth assigned to it, but pass before everything else
-        echo "#---interactive - id 100 - rate $(( $UPLINK * 1 / 10 )) kbit ceil $(( $UPLINK * 80 / 100 )) kbit"
+        rate=$(( $UPLINK * 10 / 100 ))
+        ceil=$(( $UPLINK * 80 / 100 ))
+        echo "#---interactive - id 100 - rate $rate kbit ceil $ceil kbit"
         $TC class add dev $PUBLIC_IF parent 1:1 classid 1:100 htb \
-            rate $(( $UPLINK * 10 / 100 ))kbit ceil $(( $UPLINK * 80 / 100 ))kbit \
-            burst 5k prio 1
-
+            rate ${rate}kbit ceil ${ceil}kbit \
+            burst $(( $rate * 20 / 1000 ))k cburst $(($ceil * 10/1000))k prio 1
         # this class transmits uses a pfifo before exiting
         echo "#--- ~ sub interactive: pfifo"
         $TC qdisc add dev $PUBLIC_IF parent 1:100 handle 1100: pfifo
-
         # filter definition, that will catch all the packets carrying the mark 100
         echo "#--- ~ interactive filter"
         $TC filter add dev $PUBLIC_IF parent 1:0 protocol ip prio 1 \
             handle 100 fw flowid 1:100
-
         # netfilter/iptables rules, will mark all UDP packets
         echo "#--- ~ netfilter rule - all UDP traffic at 100"
         $IPT -t mangle -A POSTROUTING -o $PUBLIC_IF -p udp -j CONNMARK --set-mark 100
+        $IPT -t mangle -A POSTROUTING -o $PUBLIC_IF -p icmp -j CONNMARK --set-mark 100
 
         # TCP ACKs class: it's important to let the ACKs leave the network as fast as possible
         # when a host of the network is downloading.
-        echo "#---tcp acks - id 200 - rate $(( $UPLINK * 0.5 )) kbit ceil $(( $UPLINK )) kbit"
+        rate=$(( $UPLINK * 7 / 10 ))
+        ceil=$(( $UPLINK ))
+        echo "#---tcp acks - id 200 - rate $rate kbit ceil $ceil kbit"
         $TC class add dev $PUBLIC_IF parent 1:1 classid 1:200 htb \
-            rate $(( $UPLINK * 0.7 ))kbit ceil $(( $UPLINK ))kbit burst 15k prio 2
+            rate ${rate}kbit ceil ${ceil}kbit burst $(($rate * 30/1000)) prio 2
         echo "#--- ~ sub tcp acks: sfq"
         $TC qdisc add dev $PUBLIC_IF parent 1:200 handle 1200: sfq perturb 10 limit 32
         echo "#--- ~ filtre tcp acks"
@@ -87,11 +89,13 @@ case "$1" in
         # DEFAULT Class: the root qdisc will send all non filtered traffic to this one
         # we also set a netfilter rule to send large file transfert to this queue
         # and avoid impacting the HTTP queue
-        echo "#---default - id 999 - rate $(( $UPLINK / 10 ))kbit ceil $(( $UPLINK ))kbit"
+        rate=$(( $UPLINK * 80 / 100 ))
+        ceil=$(( $UPLINK ))
+        echo "#---default - id 999 - rate ${rate}kbit ceil ${ceil}kbit"
         $TC class add dev $PUBLIC_IF parent 1:1 classid 1:999 htb \
-            rate $(( $UPLINK / 10 ))kbit ceil $(( $UPLINK ))kbit burst 2k prio 9
+            rate ${rate}kbit ceil ${ceil}kbit burst $((rate * 40/1000)) prio 9
         echo "#--- ~ sub default: sfq"
-        $TC qdisc add dev $PUBLIC_IF parent 1:999 handle 1999: sfq perturb 10 limit 32
+        $TC qdisc add dev $PUBLIC_IF parent 1:999 handle 1999: sfq perturb 10
         echo "#--- ~ filtre default"
         $TC filter add dev $PUBLIC_IF parent 1:0 protocol ip prio 99 handle 999 fw flowid 1:999
         $IPT -t mangle -A POSTROUTING -o $PUBLIC_IF -p tcp -m connbytes --connbytes 10000000: \
